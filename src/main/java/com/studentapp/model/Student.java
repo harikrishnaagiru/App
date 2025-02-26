bb7c530e799628dacbfd87794aaa1e02e8d02874
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-02-27 01:45:44 +0530

    Update Student.java

--- src/main/java/com/studentapp/model/Student.java
+++ src/main/java/com/studentapp/model/Student.java
@@ -11,5 +11,21 @@ public class Student {
     private String name;
     private boolean attendance;
 
-    // Getters and Setters
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter and Setter for attendance
+    public boolean isAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(boolean attendance) {
+        this.attendance = attendance;
+    }
 }
