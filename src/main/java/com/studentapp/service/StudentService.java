f8bb0c964f575be5c845e6d1ef79727bbcb4eb2e
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-02-27 00:29:06 +0530

    Create StudentService.java

---
+++ src/main/java/com/studentapp/service/StudentService.java
@@ -0,0 +1,26 @@
+package com.studentapp.service;
+
+import com.studentapp.model.Student;
+import com.studentapp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(String name) {
+        Student student = new Student();
+        student.setName(name);
+        student.setAttendance(false);
+        studentRepository.save(student);
+    }
+}
