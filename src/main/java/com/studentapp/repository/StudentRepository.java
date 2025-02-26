cec3c408ac109b30f06b4edb5fdc50d90cc950a4
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-02-27 00:20:51 +0530

    Create StudentRepository.java

---
+++ src/main/java/com/studentapp/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.studentapp.repository;
+
+import com.studentapp.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
