00176e6d7c8c1ccbbc43e136066b80f8f01c3d3e
agiruharikrishna <agiruharikrishnacloud@gmail.com>
2025-02-27 00:20:07 +0530

    Create Student.java

---
+++ src/main/java/com/studentapp/model/Student.java
@@ -0,0 +1,15 @@
+package com.studentapp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private boolean attendance;
+
+    // Getters and Setters
+}
